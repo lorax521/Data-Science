c18d6b5e6ded5844919277d8f19c97efef0fda08
James Raines <jraines521@gmail.com>
2019-03-28 13:48:06 -0600

    Update polynomial-regression.py

--- regression/polynomial-regression.py
+++ regression/polynomial-regression.py
@@ -12,20 +12,6 @@ dataset = pd.read_csv('data.csv')
 X = dataset.iloc[:, 1:2].values # matrix
 y = dataset.iloc[:, 2].values # vector
 
-# Splitting the dataset into the Training set and Test set
-'''
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
-'''
-
 #polynomial linear regression
 poly_reg = PolynomialFeatures(degree=5)
 X_poly = poly_reg.fit_transform(X)
@@ -38,7 +24,7 @@ X_grid = X_grid.reshape((len(X_grid), 1))
 
 plt.scatter(X, y, color='red')
 plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
-plt.title('True be told - Polynomial, girl')
-plt.xlabel('position')
-plt.ylabel('salary')
+plt.title('Polynomial Regression')
+plt.xlabel('Independent')
+plt.ylabel('Dependent')
 plt.show()
