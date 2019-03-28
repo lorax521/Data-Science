89216cb8a75da2a28853aa0ab3e33f829d6b4e96
James Raines <jraines521@gmail.com>
2019-03-28 13:49:48 -0600

    Update support-vector-regression.py

--- regression/support-vector-regression.py
+++ regression/support-vector-regression.py
@@ -11,12 +11,6 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Splitting the dataset into the Training set and Test set
-"""
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-"""
-
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -39,7 +33,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.title('Support Vector Regression')
+plt.xlabel('Indepedent')
+plt.ylabel('Dependent')
 plt.show()
